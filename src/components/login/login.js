5104d4a83de25fb8afdfc6b30d7bfc2ed343a389
HernanSsj <hernanns46gmail.com>
2021-01-10 20:57:47 -0300

    added more styles and custom checkbox

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,8 +2,10 @@ import React, {useState, useEffect, useCallback} from "react"
 import './login-style.css';
 import logo from '../../images/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash, faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash, faArrowRight, faCheck} from "@fortawesome/free-solid-svg-icons";
 import {faTwitter, faGoogle, faFacebook} from "@fortawesome/free-brands-svg-icons";
+import Checkbox from "react-custom-checkbox";
+
 
 const Login = ()=>{
      
@@ -12,9 +14,9 @@ const Login = ()=>{
         password: ""
 
     })
-    const [error, setError] = useState(false)
+    const [error, setError] = useState()
     const [showPassword, SetShowPassword] = useState(false)
-
+    const [stayOnline, SetstayOnline] = useState(true)
     const fillUSer =  useCallback(e => {
         
         setData({...data, user: e.target.value})
@@ -33,7 +35,10 @@ const Login = ()=>{
     const viewPassword = useCallback( e=>{
         SetShowPassword(!showPassword)
     },[showPassword])
-
+    const saveSession = useCallback( e=>{
+        SetstayOnline(!stayOnline)
+        console.log(stayOnline)
+    },[stayOnline])
     const authenticate = useCallback(data =>{
         //Dispatch auth
         //To do
@@ -44,12 +49,14 @@ const Login = ()=>{
          }
         }
       ,[data]);
+    
     const revealedPasswordIcon = <FontAwesomeIcon icon={faEye} />
     const invisiblePasswordIcon = <FontAwesomeIcon icon={faEyeSlash}/>
     const loginIcon = <FontAwesomeIcon icon={faArrowRight} size={"3x"} color={`${data.user && data.password ? "#F9F9F9"  : "#EDEDED"}`}/>
     const twitterIcon = <FontAwesomeIcon icon={faTwitter} size={"2x"} color={"#FFFFFF"}/>
     const googleIcon = <FontAwesomeIcon icon={faGoogle} size={"2x"} color={"#000000"}/>
     const facebookIcon = <FontAwesomeIcon icon={faFacebook} size={"2x"} color={"#FFFFFF"}/>
+    const stayOnlineIcon = <FontAwesomeIcon icon={faCheck} color={"#F9F9F9"}/>
     return(
         <section className="login-form">
                 <div className="container">
@@ -57,8 +64,10 @@ const Login = ()=>{
                         <h2>Iniciar sesión</h2>
                     </div>
                     <div className="input-box">
-                        <input className="form-input" placeholder="Usuario" maxlength = "16" value={data.user} onChange={fillUSer}></input>
-                        <input className="form-input" placeholder="Contraseña" maxlength = "32" value={data.password} onChange={fillPassword} type={`${showPassword ? "text"  : "password"}`}></input>
+                        <input className="username-input" required={true} maxLength = "16" value={data.user} onChange={fillUSer}></input>
+                        <label className="usr-label">Usuario</label>
+                        <input className="password-input"  required={true} maxLength = "32" value={data.password} onChange={fillPassword} type={`${showPassword ? "text"  : "password"}`}></input>
+                        <label className="pwd-label">Contraseña</label>
                         <span onClick={viewPassword}>{showPassword ? revealedPasswordIcon : invisiblePasswordIcon}</span>
                     </div>
                     <div className={`${error ? "error-message " : "invisible"}`}>
@@ -70,8 +79,26 @@ const Login = ()=>{
                         <button className="facebook">{facebookIcon}</button>
                     </div>
                     <div className="check">
-                        <input className="stay-online" type="checkbox"></input>
-                        <span>Permacener conectado</span>
+                        <Checkbox
+                        icon={
+                            <div
+                            style={{
+                              display: "flex",
+                              flex: 1,
+                              backgroundColor: "#84cdfa",
+                              borderRadius: "5px"
+                            }}
+                          >{stayOnlineIcon}
+                          </div>}
+                            name="my-input"
+                            checked={false}
+                            onChange={saveSession}
+                            borderColor="#EDEDED"
+                            borderRadius="5px"
+                            style={{ cursor: "pointer" }}
+                            labelStyle={{ marginLeft: 5, userSelect: "none" }}
+                            label="Permanecer conectado"
+                        />
                     </div>
                     <div className="buttom-box">
                         <button disabled={data.user && data.password ? false : true} onClick={authenticate}>{loginIcon}</button>

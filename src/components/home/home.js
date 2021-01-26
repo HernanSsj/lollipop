3a2b68407c02b57de1ad2a25a137dae004453ae1
HernanSsj <hernanns46gmail.com>
2021-01-26 18:59:29 -0300

    basic home layout

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,7 +3,17 @@ import './home-styles.css';
 const Home = ()=>{
     return(
         <div className="home-container">
-            <h1>Home, work in progress</h1>
+            <div className="about">
+                <div className="description">
+                    <span>Play Aniwhere Anytime</span>
+                    <h2>With Lollipop! you can watch your favorite anime episodes and movies whenever your want.</h2>
+                    <button>Get Started!</button>
+                </div>
+                   <div className="image-container">
+                     <img src="https://i.imgur.com/GOweneZ.png"></img>
+                   </div>
+                
+            </div>
         </div>
         
     )
